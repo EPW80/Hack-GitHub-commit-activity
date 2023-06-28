b28ad39c11180f9fbd3c2ea3ae6ecfcd2a9d8c93
EWP80 <erikpw009@gmail.com>
2023-06-28 00:56:59 -0700

    testing two versions of code

--- git.py
+++ git.py
@@ -1,28 +1,13 @@
-#!/usr/bin/env python3
-import subprocess
-import random
-from datetime import datetime, timedelta
+import os, random
 
-commit_messages = []
 for i in range(1000):
-    date = datetime.now() - timedelta(days=i)
-    formatted_date = date.strftime("%Y-%m-%d")
+    d = str(i) + "days ago"
     rand = random.randrange(1, 12)
-    commit_messages.append(f"{formatted_date} rand value: {rand}")
-
-with open("test.txt", "a") as file:
-    file.write("\n".join(commit_messages))
-
-subprocess.run(["git", "add", "test.txt"], check=True)
-
-for message in commit_messages:
-    try:
-        subprocess.run(
-            ["git", "commit", "--date", message.split()[0], "-m", "1"], check=True
-        )
-    except subprocess.CalledProcessError:
-        pass
-
+    with open("test.txt", "a") as file:
+        file.write(d + "\n")
+    os.system("git add test.txt")
+    os.system('git commit --date=" 2023-' + str(rand) + "-" + d + '" -m 1')
+os.system("git push -u origin main")
 
 # git commit --amend --no-edit --date="Fri Nov 6 20:00:00 2015 -0600"
 # git fetch origin master
