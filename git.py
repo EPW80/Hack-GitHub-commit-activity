cf288b3ddcd6c899f8c8928c15fb2c5e08bbfe68
EWP80 <erikpw009@gmail.com>
2023-06-28 00:48:26 -0700

    optimized code

--- git.py
+++ git.py
@@ -8,16 +8,21 @@ for i in range(400):
     date = datetime.now() - timedelta(days=i)
     formatted_date = date.strftime("%Y-%m-%d")
     rand = random.randrange(1, 12)
-    commit_messages.append(formatted_date)
+    commit_messages.append(f"{formatted_date} rand value: {rand}")
 
 with open("test.txt", "a") as file:
     file.write("\n".join(commit_messages))
+
 subprocess.run(["git", "add", "test.txt"], check=True)
 
 for message in commit_messages:
-    subprocess.run(["git", "commit", "--date", message, "-m", "1"], check=True)
+    try:
+        subprocess.run(
+            ["git", "commit", "--date", message.split()[0], "-m", "1"], check=True
+        )
+    except subprocess.CalledProcessError:
+        pass
 
-subprocess.run(["git", "push", "-u", "origin", "main"], check=True)
 
 # git commit --amend --no-edit --date="Fri Nov 6 20:00:00 2015 -0600"
 # git fetch origin master
